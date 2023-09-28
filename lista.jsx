24c8f4557d2ef0d0e2c7b0554e78d59333fead25
LadFranz <89665752+LadFranz@users.noreply.github.com>
2023-09-28 14:07:26 +0200

    Add files via upload

---
+++ lista.jsx
@@ -0,0 +1,18 @@
+const Zadatci = [
+    {ime: "oprat pod"  ,  ura : 9, obavljeno: false}, 
+    {ime: "zalit cvice"  ,   ura : 12,obavljeno: false},
+    {ime: "bacit smece"  ,    ura : 19, obavljeno: false}];
+
+
+    Zadatci.push()
+    Zadatci.pop()
+    
+   const ImaZadataka = Zadatci.includes[]
+
+    const zadatcisort = Zadatci.filter((item) => {
+        return(item.ura <= 12)})
+
+console.log(ImaZadataka)
+
+let botun = document.getElementByIdElementByID("botun")
+let kutija = document.getElementById("dodajtekst")
